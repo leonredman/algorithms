d4aea209b7f77ca507ceffa5a99cba9738be4191
leonredman <leonproart@yahoo.com>
2021-06-16 22:44:58 -0700

    init commit

---
+++ challenges/areaRectangle.js
@@ -0,0 +1,18 @@
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+
+// Examples
+// findPerimeter(6, 7) ➞ 26
+
+// findPerimeter(20, 10) ➞ 60
+
+// findPerimeter(2, 9) ➞ 22
+
+// Code
+function findPerimeter(length, width) {
+	return (length + width) * 2;
+}
+
+//Arrow function Solution
+
+
+const findPerimeter = (h, w) => (w+h)*2
