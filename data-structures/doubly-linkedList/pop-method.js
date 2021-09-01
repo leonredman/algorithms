5083c45117279a2d4f7d0636210a33169b2771f2
leonredman <leonproart@yahoo.com>
2021-08-31 22:16:19 -0700

    solution code

--- data-structures/doubly-linkedList/pop-method.js
+++ data-structures/doubly-linkedList/pop-method.js
@@ -1,4 +1,22 @@
 // POP METHOD
-
 // With doubly linked list we have arrows backwards
-// we dont have to iterate through
+// we dont have to iterate through the list as arrows point both ways
+pop() {
+
+  if (this.length === 0) return undefined
+  let temp = this.tail
+  if (this.length ==1) {
+    this.head = null
+    this.tail = null
+  } else {
+    this.tail = this.tail.prev
+    this.tail.next = null
+    temp.prev = null
+  }
+    this.length--
+    return temp
+  }
+
+  let myDoublyLinkedList = new DoublyLinkedList(1)
+  myDoublyLinkedList.push(2)
+
