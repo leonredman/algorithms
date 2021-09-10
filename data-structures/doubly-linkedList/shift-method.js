22f876e372fab40854c3402d0b8a16c3d63b0710
leonredman <leonproart@yahoo.com>
2021-09-09 23:31:30 -0700

    solution Implementation

--- data-structures/doubly-linkedList/shift-method.js
+++ data-structures/doubly-linkedList/shift-method.js
@@ -1,21 +1,25 @@
-// shift method - 
-//remove item from beginnning of list 
-
-// edge case of one item
-// no items
+// shift method - DoublyLinked List
+//This method removes an item from beginnning of list 
+// The - edge cases can bor  for  one item, and no items
 
 shift() {
+  // start as if we have an empty list 
   if (this.length == 0) return undefined
+  // then if  we have one item
   let temp = this.head
+  // if we remove the one item we have to set head and tail to null
   if ( this.length ===1) {
     this.head = null
     this.tail = null
-  } elses {
-    this.head = this.head.next
+  } elses {     // else if there is two or more items
+    this.head = this.head.next  // first we move head over one
+    // then we remove the first item breaking off head and tail
     head.prev = null
     temp.next = null
   }
+  // we decrement the list
   this.length--
+  // we return temp
   return temp
 }
 // create list
