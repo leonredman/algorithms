68729b214fcd7f8673959c92f23197b10a4afeb9
leonredman <leonproart@yahoo.com>
2021-09-21 23:06:28 -0700

    solution implementation

---
+++ data-structures/linked-list/constructor/constructor-practice.js
@@ -0,0 +1,25 @@
+//implement linked list - A WORKING CONSTRUCTOR
+// create a class with the name Node 
+class Node {
+  // create a valid constructor method and pass in a 'value' for this. Set next equal to null
+                      {
+    
+    
+  }
+}
+
+//constructor is createing new Node
+// new keyword is calling the '^ code above' constructor
+
+// class LinkedList {
+//   constructor(value) {
+//     const newNode = new Node(value);
+//     this.head = newNode;
+//     this.tail = this.head;
+//     this.length = 1;
+//   }
+// }
+
+//code creates a linked list - new LinkedList(4) calls the class linked list
+// new keyword is calling '^ code above' constructor in the class Linked List
+// let myLinkedList = new LinkedList(4);
