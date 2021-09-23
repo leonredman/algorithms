f8fa49d9c6cf7100d368a44006d0144e2b2fc493
leonredman <leonproart@yahoo.com>
2021-09-22 23:24:30 -0700

    solution Implementation

---
+++ data-structures/linked-list/shift-method/shift-practice.js
@@ -0,0 +1,25 @@
+// SHIFT METHOD
+shift(){
+  // first we check to see if list is empty
+                     // if head is null return undefined
+  //if we have 2 or more items we have to set temp variable equal to head
+  
+  //move head over to the next item
+  
+  // break connection and set temp.next equal to null
+  
+  //decrement length by one
+  
+ //all code will run until last node have to set tail equal to null as length is 0 
+                         {
+    
+  }
+
+  
+}
+// create list with 2 items 2, 1
+let myLinkedList = new LinkedList (2)
+myLinkedList.push(1)
+
+// can shift list 3 times until undefined
+myLinkedList.shift()
