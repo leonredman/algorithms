3d9f796f226ed73b3f08ab926b7ca5f33f1ec234
leonredman <leonproart@yahoo.com>
2021-10-21 19:26:47 -0700

    solutions Implement

--- data-structures/hash-tables/set-method/set-method.js
+++ data-structures/hash-tables/set-method/set-method.js
@@ -1,1 +1,24 @@
-// SET METHOD
+// SET METHOD for hash tables
+
+set(key, value) {
+  // first we find the index where we will store key value pair
+  let index = this._hash(key)
+  // we build the empty array that we will push key value pairs into
+  // we only do this step if there is not an empty array in there
+  if(!this.dataMap[index]) {
+    this.dataMap[index] = []  
+  }
+ // push in our key value pair
+ this.dataMap[index].push([key, value])
+ return this
+
+}
+// create an empty hash table
+let myHashTable = new HastTable()
+myHashTable
+// set out values
+myHashTable.set('lumber', 70)
+myHashTable.set('washers', 50)
+myHashTable.set('bolts', 1400)
+
+// we have 2 at the index of 4 and one at index of 6 if we run in dev tools console
