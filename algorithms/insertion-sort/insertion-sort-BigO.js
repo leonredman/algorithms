1a22bd1a7b2ce97a82420abd3e186c6ee95cf7bf
leonredman <leonproart@yahoo.com>
2021-12-13 09:19:15 -0700

    notes update

---
+++ algorithms/insertion-sort/insertion-sort-BigO.js
@@ -0,0 +1,24 @@
+// Big O of insertion sort
+
+// It has a for loop inside of a for loop which means its O(n^2)
+
+// If something else is almost sorted
+
+[1, 2, 4, 3, 5, 6];
+
+// In this example array, we iterate thru and see that
+// only 3 needs to be moved since it is less than
+// we basically made 1 pass. This would be n complexity not n^2
+
+// In some situations we can use other more primitive algorithms
+// like Insertion sort  but have it run faster
+
+// Bubble, Selection and insertion sort all have O(n^2) time complexity
+// Bubble, Selection and Insertion Sort all have O(1) space complexity
+// All three sort the array in place, that means you do not
+// create additional copies of the array
+
+// Bubble, Selection and insertion sort are all O(n) if you start with a sorted or almost
+// sorted array - False
+
+// Only Insertion Sort is O(n) when you start with a sorted or almost sorted data
